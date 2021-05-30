4b7b53a186ce99b7b385a57ec190176410953849
上海明馨科技有限公司 <admin@mxin.ltd>
2021-05-30 13:23:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,9 +47,9 @@ fi
 
 downloadres(){
   cd ~/Downloads
-curl -sSO https://cdn.jsdelivr.net/gh/742481030/my-share@master/fuse.pkg && echo "下载fuse成功"
+curl -sSO https://new.cy/https://github.com/742481030/my-share/raw/master/fuse.pkg && echo "下载fuse成功"
  curl -sSO  https://new.cy/https://github.com/rclone/rclone/releases/download/v1.55.1/rclone-v1.55.1-osx-amd64.zip && echo "下载myshare成功"
-curl -sSO https://cdn.jsdelivr.net/gh/742481030/my-share@master/rclone.conf && echo "下载配置文件成功"
+curl -sSO https://new.cy/https://github.com/742481030/my-share/raw/master/rclone.conf && echo "下载配置文件成功"
 ls
 
 
